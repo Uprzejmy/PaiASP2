8d288f10756d3589085a26cc0e99a83e572da755
Uprzejmy <lmerynda@gmail.com>
2017-01-18 23:48:15 +0100

    some improvements towards number generation and display

--- PaiASP2/PaiASP2/Result.aspx.cs
+++ PaiASP2/PaiASP2/Result.aspx.cs
@@ -9,9 +9,35 @@ namespace PaiASP2
 {
     public partial class Result : System.Web.UI.Page
     {
+        double start = 0.0;
+        double end = 0.0;
+        int limit;
+        int maxThreads = 1; //Liczba watkow
+        double interval;
+        Random random = new Random();
+
         protected void Page_Load(object sender, EventArgs e)
         {
             Label1.Text = Session["start"].ToString() + " " + Session["end"].ToString() + " " + Session["limit"].ToString() + " " + Session["maxThreads"].ToString();
+
+            start = Double.Parse(Session["start"].ToString());
+            end = Double.Parse(Session["end"].ToString());
+            limit = int.Parse(Session["limit"].ToString());
+            maxThreads = int.Parse(Session["maxThreads"].ToString());
+
+            interval = Math.Abs(end - start)/maxThreads;
+
+            Label1.Text = Integral(start, end, limit).ToString();
+        }
+
+        private double Integral(double start, double end, int limit)
+        {
+            return GetRandomDouble(start,end);
+        }
+        
+        private double GetRandomDouble(double minimum, double maximum)
+        {
+            return random.NextDouble() * (maximum - minimum) + minimum;
         }
     }
 }
