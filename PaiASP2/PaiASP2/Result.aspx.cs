af97497d710f7447c46b75d02ff85da40da0d7ce
Uprzejmy <lmerynda@gmail.com>
2017-01-18 23:59:52 +0100

    test

--- PaiASP2/PaiASP2/Result.aspx.cs
+++ PaiASP2/PaiASP2/Result.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -9,30 +10,51 @@ namespace PaiASP2
 {
     public partial class Result : System.Web.UI.Page
     {
-        double start = 0.0;
-        double end = 0.0;
-        int limit;
-        int maxThreads = 1; //Liczba watkow
-        double interval;
         Random random = new Random();
 
         protected void Page_Load(object sender, EventArgs e)
         {
             Label1.Text = Session["start"].ToString() + " " + Session["end"].ToString() + " " + Session["limit"].ToString() + " " + Session["maxThreads"].ToString();
 
-            start = Double.Parse(Session["start"].ToString());
-            end = Double.Parse(Session["end"].ToString());
-            limit = int.Parse(Session["limit"].ToString());
-            maxThreads = int.Parse(Session["maxThreads"].ToString());
+            double start = Double.Parse(Session["start"].ToString());
+            double end = Double.Parse(Session["end"].ToString());
+            int limit = int.Parse(Session["limit"].ToString());
+            int maxThreads = int.Parse(Session["maxThreads"].ToString());
 
-            interval = Math.Abs(end - start)/maxThreads;
-
-            Label1.Text = Integral(start, end, limit).ToString();
+            Label1.Text = Integral(start, end, limit, maxThreads).ToString();
         }
 
-        private double Integral(double start, double end, int limit)
+        private double Integral(double start, double end, int limit, int maxThreads)
         {
-            return GetRandomDouble(start,end);
+            double[] result = new double[maxThreads];
+            double interval = Math.Abs(end - start) / maxThreads;
+
+            Parallel.For(0, maxThreads, (threadId) =>
+            {
+                for (double x = threadId * interval; x < (threadId + 1) * interval; x += interval)
+                {
+                    int hitCounter = 0;
+                    for (int i = 0; i < limit; i++)
+                    {
+                        double randomX = GetRandomDouble(x, x + interval);
+                        double randomY = GetRandomDouble(-1.0, 1.0);
+                        double sin = Math.Sin(randomX);
+                        //sprawdzamy czy liczba jest pod wykresem (trafiła w przedział)
+                        if (sin < 0)
+                        {
+                            if (randomY >= sin && randomY <= 0) hitCounter++;
+                        }
+                        else
+                        {
+                            if (randomY <= sin && randomY >= 0) hitCounter++;
+                        }
+                    }
+                    //zapisujemy wynik dla danego wątku
+                    result[threadId] = hitCounter;
+                }
+            });
+            //zwracamy obliczoną całkę
+            return result.Sum()/(limit*maxThreads);
         }
         
         private double GetRandomDouble(double minimum, double maximum)
